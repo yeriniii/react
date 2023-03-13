8db24f53ec71fb76f1a94786f83e937631a6fedc
yeriniii <yerini000303@naver.com>
2023-03-14 00:32:45 +0900

    state gramm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; //css경로
+import { useState } from 'react'; //state문법
 
 function App() {
+
+  let post = 'react란?';
+  let [title, b] = useState( ['Day1','Day2','Day3'] );
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={ {color:'blue', fontSize:'20px'} }>reactprac</h4>
+      </div>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>0314</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>0314</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>0314</p>
+      </div>
+
+      <h4>{ post }</h4>
     </div>
   );
 }
